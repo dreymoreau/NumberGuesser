d0c80ab3f825b04e3f77f4aea2fd2ee205b58ced
dreymotoh <striclyspoken@gmail.com>
2023-07-21 15:15:09 -0400

    functionality added for number guessing game, differentiating colours when user guessed the wrong number and the right number

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -23,6 +23,54 @@ namespace NumberGuesser
 
             // reset text colour after app info
             Console.ResetColor();
+
+            // Ask users name
+            Console.WriteLine("What is your name?");
+
+            // Get user input
+            string inputName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, lets play a game!", inputName);
+
+            // Initialize correct number
+            int correctNumber = 7;
+
+            // Initialize guess variable
+            int guess = 0;
+
+            // ask user for number
+            Console.WriteLine("Guess a number between 1 and 10");
+            
+            // while guess is not correct
+            while (guess != correctNumber) {
+                // get user input
+                string input = Console.ReadLine();
+
+                //take input and parse string into a int and put into
+                // guess variable
+
+                guess = Int32.Parse(input);
+
+                //Match guess to correct number
+                if (guess != correctNumber) {
+                    // change text colour to red for error message
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    // Write out error message
+                    Console.WriteLine("Wrong number, please try again");
+
+                    // reset text colour after app info
+                    Console.ResetColor();
+                }
+            }
+            // change text colour to red for error message
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            // Tell User they chose the right number
+            Console.WriteLine("You are correct!");
+
+            // reset text colour after app info
+            Console.ResetColor();
         }
     }
 }
