24004e6d00462f0c80b628374135a2d6279a8e1a
dreymotoh <striclyspoken@gmail.com>
2023-07-21 14:41:32 -0400

    Add project files.

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+//namespace
+namespace NumberGuesser
+{
+    //main class
+    class Program
+    {
+        // entry point
+        static void Main(string[] args)
+        {
+            string name = "Drey";
+            int age = 25;
+
+            // START HERE, WriteLine creates a new line, you can do Line 
+            // but the rest of the output would be on the same line
+            Console.WriteLine(name + " is " +age);
+
+        }
+    }
+}
+
+
